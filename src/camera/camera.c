2d8f4c36ba40afc41d0d7730372af8d0df7ba9d3
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-03-16 16:16:43 +0200

    fixed the shading.

--- src/camera/camera.c
+++ src/camera/camera.c
@@ -14,7 +14,7 @@ unsigned int	get_color(t_vec3 vec)
 
 void	break_point(int i)
 {
-	printf("I am here! %d", i);
+	i += 1;
 	return 	;
 }
 
@@ -34,7 +34,7 @@ void	camera_render(t_info *info)
 		i = 0;
 		while (i < info->c.image_width)
 		{
-			if (j == 547 && i == 1140)
+			if (j == 618 && i == 1007)
 				break_point(i);
 			ray = camera_get_ray(&info->c, i, j);
 			color = camera_ray_color(info, ray, &info->obj, MAX_DEPTH);

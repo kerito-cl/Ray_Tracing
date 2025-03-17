9e46c0faa0a1c72e3f5cbdd5c1b8d7a845df8971
Matias Quero <mquero@c1r6p19.hive.fi>
2025-03-17 18:04:07 +0200

    cylinder done

--- src/objects/cylinder.c
+++ src/objects/cylinder.c
@@ -1,55 +1,72 @@
 
 #include "mini_rt.h"
 
-static float quadratic(float a, float b, float c)
+bool quadratic(float a, float b, float c, float *t0, float *t1)
 {
-    float discriminant;
-    float t0;
-    float t1;
-
-    discriminant = (b * b) - (4 * a * c);
+    float discriminant = (b * b) - (4 * a * c);
     if (discriminant < 0)
-        return (INFINITY);
+        return false;  // No real roots
+
     discriminant = sqrtf(discriminant);
-    t0 = (-b - discriminant) * (1 / (2 * a));
-    t1 = (-b + discriminant) * (1 / (2 * a));
-    if (t0 > 0)
-        return (t0);
-    if (t1 > 0)
-        return (t1);
-    return (INFINITY);
+    float inv2a = 0.5 / a;  // More precise division
+    *t0 = (-b - discriminant) * inv2a;
+    *t1 = (-b + discriminant) * inv2a;
+
+    return true;
 }
 
-bool	cy_hit(t_obj *cy, t_ray *ray, t_interval *interval, t_hit_record *rec)
+bool cy_hit(t_obj *cy, t_ray *ray, t_interval *interval, t_hit_record *rec)
 {
-    t_vec3 l;
-    float a;
-    float b;
-    float c;
+    // Construct local basis
+    /*t_vec3 x = vec3_sub_vecs(ray->orig, cy->point);
+    float d_dot = vec3_dot(ray->direc, cy->normal);
+    float x_dot = vec3_dot(x, cy->normal);
 
-    
-    l = vec3_copy(ray->orig);
-    l = vec3_sub_vecs(l, cy->point);
-    a = vec3_dot(ray->direc, ray->direc);
-    b = 2 * vec3_dot(l, ray->direc);
-    c = vec3_dot(l, l) - (cy->radius * cy->radius);
-    rec->t = quadratic(a, b, c);
-    rec->p = ray_at(ray, rec->t);
-    if (rec->t != INFINITY)
+    t_vec3 d_proj = vec3_mul_vec(cy->normal, d_dot);
+    t_vec3 x_proj = vec3_mul_vec(cy->normal, x_dot);
+
+    t_vec3 d_perp = vec3_sub_vecs(ray->direc, d_proj);
+    t_vec3 x_perp = vec3_sub_vecs(x, x_proj);
+
+    float a = vec3_dot(d_perp, d_perp);
+    float b = 2.0 * vec3_dot(d_perp, x_perp);
+    float c = vec3_dot(x_perp, x_perp) - (cy->radius * cy->radius);*/
+
+    t_vec3 x = vec3_sub_vecs(ray->orig, cy->point);
+    float d_dot = vec3_dot(ray->direc, cy->normal);
+    float x_dot = vec3_dot(x, cy->normal);
+
+    float a = vec3_dot(ray->direc, ray->direc) - (d_dot * d_dot);
+    float b = 2.0 * (vec3_dot(ray->direc, x) - (d_dot * x_dot));
+    float c = vec3_dot(x, x) - (x_dot * x_dot) - (cy->radius * cy->radius);
+    float t0, t1;
+    if (t0 > t1) { float temp = t0; t0 = t1; t1 = temp; }
+
+    // Ignore negative intersections (behind the ray)
+    if (t0 < 0) t0 = t1;
+    if (t0 < 0) return false;
+
+    if (!quadratic(a, b, c, &t0, &t1))
+        return false; // No intersection
+    for (int i = 0; i < 2; i++)
     {
-	    rec->material = &cy->material;
-        cy->normal = vec3_sub_vecs(rec->p, cy->point);
-        rec->normal.y = 0;
-        a = vec3_dot(rec->normal, ray->direc);
-        if (a > 0)
-            rec->normal =  vec3_mul_vec(rec->normal, -1);
-        printf("%f\n", rec->t);
-        return (true);
-    }
-    return (false);
-}  
-/*bool	cy_hit(t_obj *cy, t_ray *ray, t_interval interval, t_hit_record *rec)
-{
-    
+        rec->t = (i == 0) ? t0 : t1;
+        float m = d_dot * rec->t + x_dot;
+        if (m >= 0.0 && m <= cy->height)
+        {
+            if (!interval_surrounds(interval, rec->t))
+                return false;
+            rec->p = ray_at(ray, rec->t);
 
-}*/
+            rec->material = &cy->material;
+            t_vec3 a = vec3_add_vecs(cy->point, vec3_mul_vec(cy->normal, m));
+            rec->normal = vec3_sub_vecs(rec->p, a);
+            rec->normal = vec3_unit(rec->normal);
+            if (vec3_dot(rec->normal, ray->direc) > 0)
+                rec->normal = vec3_mul_vec(rec->normal, -1);
+            return true;
+        }
+    }
+    return false;
+}
+

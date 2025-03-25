4420850e39c34d98f31653c77a0c807e0c110e36
Kero <matias.quero@gmail.com>
2025-03-25 20:07:24 +0200

    fix plane bug and cylinder norminette

--- src/objects/plane.c
+++ src/objects/plane.c
@@ -8,19 +8,19 @@ bool	pl_hit(t_obj *plane, t_ray *ray, t_interval *interval,
 	float result;
 
 	dot_plane = vec3_dot(ray->direc, plane->normal);
-	if (dot_plane >= 0)
+	if (dot_plane == 0)
 		return (false);
 	plane_ray_vec = vec3_copy(plane->point);
 	plane_ray_vec = vec3_sub_vecs(plane_ray_vec, ray->orig);
 	result = vec3_dot(plane_ray_vec, plane->normal);
 	result = result / dot_plane;
-	if (result > interval->min && result < interval->max)
-	{
-		rec->t = result;
-		rec->p = ray_at(ray, result);
-		rec->normal = plane->normal;
-		rec->material = &plane->material;
-		return (true);
-	}
-	return (false);
+	if (!interval_surrounds(interval, result))
+		return (false);
+	rec->t = result;
+	rec->p = ray_at(ray, result);
+	rec->normal = plane->normal;
+	if (vec3_dot(rec->normal, ray->direc) > 0)
+		rec->normal = vec3_mul_vec(rec->normal, -1);
+	rec->material = &plane->material;
+	return (true);
 }

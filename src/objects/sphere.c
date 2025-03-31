b682e61effdbd43612fce75e9439e692f26c65ce
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-03-31 13:00:07 +0300

    fixed sphere.

--- src/objects/sphere.c
+++ src/objects/sphere.c
@@ -1,40 +1,6 @@
 #include "mini_rt.h"
 #include "vars.h"
 
-bool	sp_hit2(t_obj *sphere, t_ray *ray, t_interval *interval,
-		t_hit_record *rec)
-{
-	t_sp_hit_vars	vars;
-
-	if (sphere->material.type_material == LIGHT && ((!LIGHT_VISIBLE
-				&& ray->type == CAM_RAY) || ray->type == REFECTION_RAY))
-		return (false);
-	vars.oc = vec3_sub_vecs(sphere->point, ray->orig);
-	vars.a = vec3_length_squared(ray->direc);
-	vars.c = vec3_length_squared(vars.oc) - sphere->radius * sphere->radius;
-	vars.h = vec3_dot(ray->direc, vars.oc);
-	vars.discriminant = vars.h * vars.h - vars.a * vars.c;
-	if (vars.discriminant < 0)
-		return (false);
-	vars.sqrtd = sqrtf(vars.discriminant);
-	vars.root = (vars.h - vars.sqrtd) / vars.a;
-	if (!interval_surrounds(interval, vars.root))
-	{
-		vars.root = (vars.h + vars.sqrtd) / vars.a;
-		if (!interval_surrounds(interval, vars.root))
-			return (false);
-	}
-	rec->t = vars.root;
-	rec->p = ray_at(ray, vars.root);
-	vars.outward_normal = vec3_div_vec(vec3_sub_vecs(rec->p, sphere->point),
-			sphere->radius);
-	rec->material = &sphere->material;
-	set_face_normal(*ray, vars.outward_normal, rec);
-	rec->u = (atan2f(-vars.outward_normal.z, vars.outward_normal.x) + PI) / (2 * PI);
-	rec->v = acosf(-vars.outward_normal.y) / PI;
-	return (true);
-}
-
 // @details returns if the given sphere is hit by the given ray in given interval.
 // and fill the given rec.
 //
@@ -67,7 +33,7 @@ bool	sp_hit(t_obj *sphere, t_ray *ray, t_interval *interval,
 	var.a = vec3_length_squared(ray->direc);
 	var.b = 2.0f * vec3_dot(ray->direc, var.oc);
 	var.c = vec3_length_squared(var.oc) - sphere->radius * sphere->radius;
-	if (!quadratic(var.a, var.b, var.c, var.t))
+	if (!cal_quadratic(var.a, var.b, var.c, var.t))
 		return false;
 	if (interval_surrounds(interval, var.t[0]))
 		rec->t = var.t[0];

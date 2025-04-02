6455ae171b6e40c60ff02daa607682db6bd1c25b
Kero <matias.quero@gmail.com>
2025-04-02 20:18:15 +0300

    test scene

--- include/constants.h
+++ include/constants.h
@@ -5,7 +5,7 @@
 # define ROTATE_STEP DTR(10)
 # define LIGHT_VISIBLE 1
 # define LIGHT_RADIUS 1
-# define METAL_FUZZ 0.5f
+# define METAL_FUZZ 0.1f
 # define METAL_BR_RATIO 0.5f
 # define GLASS_REF_IDX 1.5f
 # define AIR_REF_IDX 1.3f / 1.5f
